e365ad891a6ca724541d9f30395588d1a555c931
Joseph Suh <jsuh@isi.edu>
2012-05-31 18:27:11 -0400

    schedule_create_volumne added

--- nova/scheduler/filter_scheduler.py
+++ nova/scheduler/filter_scheduler.py
@@ -261,3 +261,36 @@ class FilterScheduler(driver.Scheduler):
 
         self.cost_function_cache[topic] = cost_fns
         return cost_fns
+
+    def schedule_create_volume(self, context, volume_id, *_args, **_kwargs):
+        """Picks a host that is up and has the fewest volumes."""
+        elevated = context.elevated()
+
+        volume_ref = db.volume_get(context, volume_id)
+        availability_zone = volume_ref.get('availability_zone')
+
+        zone, host = None, None
+        if availability_zone:
+            zone, _x, host = availability_zone.partition(':')
+        if host and context.is_admin:
+            service = db.service_get_by_args(elevated, host, 'nova-volume')
+            if not utils.service_is_up(service):
+                raise exception.WillNotSchedule(host=host)
+            driver.cast_to_volume_host(context, host, 'create_volume',
+                    volume_id=volume_id, **_kwargs)
+            return None
+
+        results = db.service_get_all_volume_sorted(elevated)
+        if zone:
+            results = [(service, gigs) for (service, gigs) in results
+                       if service['availability_zone'] == zone]
+        for result in results:
+            (service, volume_gigabytes) = result
+            if volume_gigabytes + volume_ref['size'] > FLAGS.max_gigabytes:
+                msg = _("Not enough allocatable volume gigabytes remaining")
+                raise exception.NoValidHost(reason=msg)
+            if utils.service_is_up(service) and not service['disabled']:
+                driver.cast_to_volume_host(context, service['host'],
+                        'create_volume', volume_id=volume_id, **_kwargs)
+                return None
+        msg = _("Is the appropriate service running?")
